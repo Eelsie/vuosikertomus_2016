a1b5379617c543824a9bd94cd6d5ea4f2d6473d5
Bestetti, Elisa <Elisa.Bestetti@plan-international.org>
2016-12-10 09:22:48 +0200

    9.12.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
 
-
-// logo fade out
-  $(window).scroll(function(){
-    $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
-
-    var wScroll = $(this).scrollTop();
-
-    if(wScroll > $('#tytot').offset().top - ($(window).height() / 1.2)) {
-      $('#tytot').addClass('showing');
-    }
-
-  });
-
 // overlay menu
   $("#menu").click(function(){
     $(".overlay").fadeToggle(200);
@@ -22,6 +9,30 @@ $(document).ready(function(){
     $(".overlay").fadeToggle(200);
     $("#menu").toggleClass('btn-open').toggleClass('btn-close');
   });
+
+// logo fade out
+  $(window).scroll(function(){
+    $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
+
+    var wScroll = $(this).scrollTop();
+
+    $('header').css('background-position', 'center ' + -wScroll/3 + 'px');
+
+
+// elements fade in and slide from bottom
+    if($('.logo').css('width') == '160px') {
+      if(wScroll > $('#tytot').offset().top - ($(window).height() / 1.2)) {
+        $('#tytot').addClass('showing');
+      }
+      if(wScroll > $('#humanitaarinen').offset().top - ($(window).height() / 1.2)) {
+        $('#humanitaarinen').addClass('showing');
+      }
+    }
+
+
+  });
+
+
 
 
 
