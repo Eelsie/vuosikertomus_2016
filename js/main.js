e937230ff92601ab624b6e68c4bd02ebc6b47c2a
Bestetti, Elisa <Elisa.Bestetti@plan-international.org>
2016-12-13 09:27:51 +0200

    12.12.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+// force scroll to top on refresh
+$(window).scrollTop(0);
+
+// avoid logo to appear on refresh if scroll != 0
+  // if ($(window).scrollTop() > 40) {
+  //   $(".logo").css("opacity", 0);
+  // }
+
+// fadein ONLOAD
+  $(function() {
+    $('body').removeClass('fade-out');
+  });
+
 // overlay menu
   $("#menu").click(function(){
     $(".overlay").fadeToggle(200);
@@ -10,24 +23,41 @@ $(document).ready(function(){
     $("#menu").toggleClass('btn-open').toggleClass('btn-close');
   });
 
-// logo fade out
+// all what happens on scroll
   $(window).scroll(function(){
-    $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
-
     var wScroll = $(this).scrollTop();
 
+// logo fade out
+    $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
+
+// parallax of header image
     $('header').css('background-position', 'center ' + -wScroll/3 + 'px');
 
-
 // elements fade in and slide from bottom
-    if($('.logo').css('width') == '160px') {
-      if(wScroll > $('#tytot').offset().top - ($(window).height() / 1.2)) {
-        $('#tytot').addClass('showing');
-      }
-      if(wScroll > $('#humanitaarinen').offset().top - ($(window).height() / 1.2)) {
-        $('#humanitaarinen').addClass('showing');
+    function slidingBottom(el) {
+      if(wScroll > $(el).offset().top - ($(window).height() / 1.2)) {
+        $(el).addClass('showing');
       }
     }
+    function slidingBottomImg(el, className) {
+      if(wScroll > $(el).offset().top - ($(window).height() / 1.2)) {
+        $(el).addClass(className);
+      }
+    }
+
+    if($('.logo').css('width') === '160px') {
+      // slidingBottom('#intro');
+      slidingBottom('#tytot');
+      slidingBottom('#humanitaarinen');
+      slidingBottomImg('.resp-img:first-child', 'dia-showing01');
+      slidingBottomImg('.resp-img:nth-child(2)', 'dia-showing02');
+      slidingBottomImg('.resp-img:nth-child(3)', 'dia-showing03');
+      slidingBottomImg('.resp-img:nth-child(4)', 'dia-showing04');
+      slidingBottomImg('.resp-img:nth-child(5)', 'dia-showing05');
+
+    }
+
+
 
 
   });
