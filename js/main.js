8244cf6acf0c8b86df24a357b064700656716c71
Bestetti, Elisa <Elisa.Bestetti@plan-international.org>
2017-02-27 11:23:17 +0200

    Added English page

--- js/main.js
+++ js/main.js
@@ -1,60 +1,46 @@
 // video play
 
 function play(){
-  document.getElementById('video1').innerHTML = '<iframe  src="http://www.youtube.com/embed/QdwOBDADbRA?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video1').innerHTML = '<iframe  src="https://www.youtube.com/embed/QdwOBDADbRA?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play2(){
-  document.getElementById('video2').innerHTML = '<iframe  src="http://www.youtube.com/embed/fuDwmNlZfMo?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video2').innerHTML = '<iframe  src="https://www.youtube.com/embed/fuDwmNlZfMo?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play3(){
-  document.getElementById('video3').innerHTML = '<iframe  src="http://www.youtube.com/embed/u46RcoqPz4Y?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video3').innerHTML = '<iframe  src="https://www.youtube.com/embed/u46RcoqPz4Y?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play4(){
-  document.getElementById('video4').innerHTML = '<iframe  src="http://www.youtube.com/embed/bpO7ev2OsVI?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video4').innerHTML = '<iframe  src="https://www.youtube.com/embed/bpO7ev2OsVI?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play5(){
-  document.getElementById('video5').innerHTML = '<iframe  src="http://www.youtube.com/embed/9eqm8i3T9l4?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video5').innerHTML = '<iframe  src="https://www.youtube.com/embed/9eqm8i3T9l4?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play6(){
-  document.getElementById('video6').innerHTML = '<iframe  src="http://www.youtube.com/embed/3BVj4-vObb8?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video6').innerHTML = '<iframe  src="https://www.youtube.com/embed/3BVj4-vObb8?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play7(){
-  document.getElementById('video7').innerHTML = '<iframe  src="http://www.youtube.com/embed/SVzUi66xBg8?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video7').innerHTML = '<iframe  src="https://www.youtube.com/embed/SVzUi66xBg8?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play8(){
-  document.getElementById('video8').innerHTML = '<iframe  src="http://www.youtube.com/embed/fRsnrwfOI3Q?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video8').innerHTML = '<iframe  src="https://www.youtube.com/embed/fRsnrwfOI3Q?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 function play9(){
-  document.getElementById('video9').innerHTML = '<iframe  src="http://www.youtube.com/embed/n8qoJ9-W0p0?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+  document.getElementById('video9').innerHTML = '<iframe  src="https://www.youtube.com/embed/n8qoJ9-W0p0?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
 }
 
 
 $(document).ready(function(){
 //
-//   if( /Android|webOS|iPhone|iPad/i.test(navigator.userAgent) ) {
-//     $("div.col.columns").addClass('col-mobile');
-//     $("div.col.title").addClass('col-mobile');
-//     $("div.col").removeClass('columns');
-//   }
-//
-//   var ua = navigator.userAgent.toLowerCase();
-// if (ua.indexOf('safari') != -1) {
-//   if (ua.indexOf('chrome') > -1) {
-//
-//   } else {
-//     $("div.col.columns").addClass('col-mobile');
-//     $("div.col.title").addClass('col-mobile');
-//     $("div.col").removeClass('columns');
-//   }
-// }
+
+
 
 
 // force scroll to top on refresh
@@ -80,17 +66,22 @@ $(window).scrollTop(0);
     $("#menu").toggleClass('btn-open').toggleClass('btn-close');
   });
 
+
 // all what happens on scroll
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
 
-// logo fade out
+// logo and language link fade out
     $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
+    $("#lang").css("opacity", 1 - $(window).scrollTop() / 250);
+
 
 // parallax of header image
     if ($(window).width() > 1400){
       $('header').css('background-position', 'center ' + -wScroll/3 + 'px');
     }
+
+
 
 // elements fade in and slide from bottom
     function slidingBottom(el) {
@@ -98,6 +89,7 @@ $(window).scrollTop(0);
         $(el).addClass('showing');
       }
     }
+
     function slidingBottomImg(el, className) {
       if(wScroll > $(el).offset().top - ($(window).height() / 1.2)) {
         $(el).addClass(className);
@@ -132,5 +124,24 @@ $(window).scrollTop(0);
       slidingBottom('#talous');
       slidingBottom('#jh');
   });
+
+
+  // avoid hiding of titles when menu items clicked
+
+    // The function actually applying the offset
+    function offsetAnchor() {
+        if(location.hash.length !== 0) {
+            window.scrollTo(window.scrollX, window.scrollY - 80);
+        }
+    }
+
+    // This will capture hash changes while on the page
+    window.addEventListener("hashchange", offsetAnchor);
+
+    // if( /iPhone|iPad/i.test(navigator.userAgent) ) {
+    //   $("#infograffa").addClass('no-showing');
+    //   $("#infograffa2").addClass('no-showing');
+    //
+    // }
 
 });
